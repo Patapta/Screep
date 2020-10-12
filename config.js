0544a24d5e1f061b12421e499ed8db0300884cce
Aoi <Patapataboom@aliyun.com>
2020-10-12 14:09:27 +0800

    修改虫子配置

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // towers : ['f538e51e7b03e17', 'e4236189e5c0097'],
     harvesters_number : 5,//劳作型虫子数量
-    upgraders_number : 5,//升级型虫子数量
+    upgraders_number : 7,//升级型虫子数量
     builders_in_need : 10,//有建筑物待建时建造型虫子数量
     buliders_idle : 0,//没有建筑物待建时摸鱼型虫子数量
 }
