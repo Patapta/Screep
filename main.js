748da13f87ec8a6c9ab421f7c89d4d466e16d5b1
Aoi <Patapataboom@aliyun.com>
2020-10-12 11:25:05 +0800

    test

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ module.exports.loop = function () {
     arr['age'] = 12;
     arr['name'] = 'Chaos';
     console.log(arr);
-    console.log(typeof arr);
+    console.log(arr instanceof Array);
     
     for(var name in Memory.creeps){
         if(!Game.creeps[name]){
