b647200f1b3e5b6661145071871597622b7d6467
Aoi <Patapataboom@aliyun.com>
2020-10-13 10:36:56 +0800

    test

--- role.harvester.js
+++ role.harvester.js
@@ -32,7 +32,7 @@ var roleHarvester = {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
-            console.log(work_targets[0].structureType);
+            console.log('充能目标'+work_targets[0].structureType);
             //如果没有充能目标
             if(creep.memory.targetId == null){
                 creep.memory.targetId = work_targets[0].id;
