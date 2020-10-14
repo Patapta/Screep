c97663232233140e4ead6041aad6091a4b9779ee
Aoi <Patapataboom@aliyun.com>
2020-10-14 10:11:36 +0800

    修改虫子比例

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     // towers : ['f538e51e7b03e17', 'e4236189e5c0097'],
     harvesters_number : 7,//劳作型虫子数量
     upgraders_number : 5,//升级型虫子数量
-    builders_in_need : 4,//有建筑物待建时建造型虫子数量
+    builders_in_need : 1,//有建筑物待建时建造型虫子数量
     buliders_idle : 0,//没有建筑物待建时摸鱼型虫子数量
 }
