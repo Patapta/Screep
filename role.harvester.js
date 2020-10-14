596bc94d0e5c7807de77b090d42d8205a90942d3
Aoi <Patapataboom@aliyun.com>
2020-10-14 11:47:49 +0800

    当充能目标为storage时每次按照优先级重新制定充能目标

--- role.harvester.js
+++ role.harvester.js
@@ -34,6 +34,7 @@ var roleHarvester = {
             });
             //如果没有充能目标
             if(creep.memory.targetId == null){
+                creep.memory.targetId = work_targets[0].id;
                 this.charge_order(work_targets, creep);
             }
             var work_target = Game.getObjectById(creep.memory.targetId);
@@ -43,6 +44,7 @@ var roleHarvester = {
                 }
                 //如果目标建筑物能量满了
                 if(work_target.store.getFreeCapacity(RESOURCE_ENERGY) == 0){
+                    creep.memory.targetId = work_targets[0].id;
                     this.charge_order(work_targets, creep);
                 }
             }
