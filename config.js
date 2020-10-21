9048deb9339bc460eed5110a7207704d691b4eb5
Ah <Patapataboom@aliyun.com>
2020-10-21 22:21:28 +0800

    紧急修复

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     harvesters_number : 7,//劳作型虫子数量
     upgraders_number : 5,//升级型虫子数量
-    builders_in_need : 1,//有建筑物待建时建造型虫子数量
+    builders_in_need : 5,//有建筑物待建时建造型虫子数量
     buliders_idle : 0,//没有建筑物待建时摸鱼型虫子数量
     harvesters_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
     upgraders_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
