5ce006b831f538d130b226e7c5d7f742b8877152
Ah <Patapataboom@aliyun.com>
2020-10-22 21:11:18 +0800

    被干了

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     upgraders_number : 5,//升级型虫子数量
     builders_in_need : 5,//有建筑物待建时建造型虫子数量
     buliders_idle : 0,//没有建筑物待建时摸鱼型虫子数量
-    harvesters_components : [[WORK, 1], [CARRY, 1], [MOVE, 1]],
+    harvesters_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
     upgraders_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
     builders_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
 }
