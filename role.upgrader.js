ce4397b14f6e358e4851151b46a6b6542d3cae94
Aoi <Patapataboom@aliyun.com>
2020-10-24 14:22:11 +0800

    临时更改升级工人取矿位置

--- role.upgrader.js
+++ role.upgrader.js
@@ -23,12 +23,13 @@ var roleUpgrader = {
         }
         else{
             //upgrader get energy temporarily
-            var sources1 = Game.spawns['Home1'].room.find(FIND_STRUCTURES, {
+            var storage = Game.spawns['Home1'].room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_STORAGE);
                 }
             });
-            console.log(sources1);
+            var target = Game.getObjectById(storage[0].id);
+            console.log(target);
             if(creep.harvest(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id)) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id), {visualizePathStyle: {stroke: '#ff0000'}});
             }
