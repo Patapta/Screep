da92f33d6bf283fdf053f9b11ad4ff0ecd6f1717
Ah <Patapataboom@aliyun.com>
2020-10-25 17:55:10 +0800

    升级工人从storage取矿物

--- role.upgrader.js
+++ role.upgrader.js
@@ -22,19 +22,19 @@ var roleUpgrader = {
             }
         }
         else{
-            // //upgrader get energy temporarily
-            // var storage = Game.spawns['Home1'].room.find(FIND_STRUCTURES, {
-            //     filter: (structure) => {
-            //         return (structure.structureType == STRUCTURE_STORAGE);
-            //     }
-            // });
-            // if(creep.withdraw(storage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            //     creep.moveTo(storage[0], {visualizePathStyle: {stroke: '#ff0000'}});
-            // }
-            // upgrader get energy by themselves
-            if(creep.harvest(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id)) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id), {visualizePathStyle: {stroke: '#ff0000'}});
+            //upgrader get energy temporarily
+            var storage = Game.spawns['Home1'].room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_STORAGE);
+                }
+            });
+            if(creep.withdraw(storage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(storage[0], {visualizePathStyle: {stroke: '#ff0000'}});
             }
+            // // upgrader get energy by themselves
+            // if(creep.harvest(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id)) == ERR_NOT_IN_RANGE) {
+            //     creep.moveTo(Game.getObjectById(creep.pos.findClosestByRange(FIND_SOURCES).id), {visualizePathStyle: {stroke: '#ff0000'}});
+            // }
         }
     }
 }
