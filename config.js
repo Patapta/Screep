a35a8dea17c2e35c0f740fc3aa9cc47d2dedf66b
Ah <Patapataboom@aliyun.com>
2020-10-26 21:24:55 +0800

    修改弔虫部件

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     builders_in_need : 2,//numbers of creep when there are buildings in need
     buliders_idle : 0,//                               not
     //set the parts of difference creeps
-    harvesters_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
-    upgraders_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
-    builders_components : [[WORK, 4], [CARRY, 1], [MOVE, 5]],
+    harvesters_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
+    upgraders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
+    builders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
 }
