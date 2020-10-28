7e2d9923983f0db63b49cd01a89982bab3f8a9ea
Ah <Patapataboom@aliyun.com>
2020-10-28 21:21:40 +0800

    修改建筑弔虫数量

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     harvesters_number : 5,
-    upgraders_number : 1,
+    upgraders_number : 2,
     builders_in_need : 2,//numbers of creep when there are buildings in need
     buliders_idle : 0,//                               not
     //set the parts of difference creeps
