2017a41a21a73cb4115fbfc21592fa2750276d2e
Ah <Patapataboom@aliyun.com>
2020-10-28 22:30:22 +0800

    优化获取房间全部建筑

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ module.exports.loop = function () {
             Game.structures[key].structureType == STRUCTURE_TOWER ||
             Game.structures[key].structureType == STRUCTURE_STORAGE){
             structures.push(Game.structures[key]);
+            console.log(Game.structures[key]);
         }
-        console.log(Game.structures[key]);
         // console.log(Game.structures[key].store.getFreeCapacity(RESOURCE_ENERGY));
     }
 
