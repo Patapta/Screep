51185851f83a068846632c18e57a3a0dcc0a0dd7
Aoi <Patapataboom@aliyun.com>
2020-10-29 14:57:57 +0800

    紧急修复

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     builders_in_need : 2,//numbers of creep when there are buildings in need
     buliders_idle : 0,//                               not
     //set the parts of difference creeps
-    harvesters_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
+    harvesters_components : [[WORK, 3], [CARRY, 1], [MOVE, 4]],
     upgraders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
     builders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
 }
