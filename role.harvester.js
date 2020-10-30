984a82c545ccaeb3a5c6a802b9dc99905beec8d4
Aoi <Patapataboom@aliyun.com>
2020-10-30 11:01:55 +0800

    test计算最近充能目标

--- role.harvester.js
+++ role.harvester.js
@@ -80,18 +80,39 @@ var roleHarvester = {
             if (work_targets[i].structureType == STRUCTURE_SPAWN) {
                 creep.memory.targetId = work_targets[i].id;
                 return 1;
+                let sort = [];
+                sort.push(work_targets[i]);
+                let closest_structure = common.closest_structure(sort, creep);
+                console.log(closest_structure);
+                console.log(work_targets[i]);
+                // creep.memory.targetId = closest_structure.id;
+                // return 1;
             }
         }
         for (let i = 0;i < work_targets.length;i++){
             if (work_targets[i].structureType == STRUCTURE_TOWER) {
                 creep.memory.targetId = work_targets[i].id;
                 return 1;
+                let sort = [];
+                sort.push(work_targets[i]);
+                let closest_structure = common.closest_structure(sort, creep);
+                console.log(closest_structure);
+                console.log(work_targets[i]);
+                // creep.memory.targetId = closest_structure.id;
+                // return 1;
             }
         }
         for (let i = 0;i < work_targets.length;i++){
             if (work_targets[i].structureType == STRUCTURE_STORAGE) {
                 creep.memory.targetId = work_targets[i].id;
                 return 1;
+                let sort = [];
+                sort.push(work_targets[i]);
+                let closest_structure = common.closest_structure(sort, creep);
+                console.log(closest_structure);
+                console.log(work_targets[i]);
+                // creep.memory.targetId = closest_structure.id;
+                // return 1;
             }
         }
     },
