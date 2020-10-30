cbddaf8f12c45ca3aa44836feb0257c57fd4aa1d
Aoi <Patapataboom@aliyun.com>
2020-10-30 15:27:20 +0800

    test计算最近充能目标优化，建造工人从storage里取建造材料

--- role.harvester.js
+++ role.harvester.js
@@ -69,7 +69,7 @@ var roleHarvester = {
      * @returns {number}
      */
     charge_order : function (work_targets, creep) {
-        charge order extension>spawn>tower>storage
+        // charge order extension>spawn>tower>storage
         let check = this.filter_structure(work_targets, 'STRUCTURE_EXTENSION', creep);
         if (check == 1){
             return 1;
