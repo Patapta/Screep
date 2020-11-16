116173df3877f21fdf14f90855212f0e3ec54845
Ah <Patapataboom@aliyun.com>
2020-11-16 20:54:49 +0800

    修改弔虫数量配置

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    harvesters_number : 5,
-    upgraders_number : 3,
+    harvesters_number : 4,
+    upgraders_number : 2,
     builders_in_need : 2,//numbers of creep when there are buildings in need
     buliders_idle : 0,//                               not
     //set the parts of difference creeps
-    harvesters_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
+    harvesters_components : [[WORK, 10], [CARRY, 5], [MOVE, 15]],
     emergency_harvesters_components : [[WORK, 1], [CARRY, 1], [MOVE, 1]],
     upgraders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
     builders_components : [[WORK, 8], [CARRY, 5], [MOVE, 13]],
